107d55f635ab625ef3486671a5455d7f6927eb02
Ramdoni007 <ramd42156@gmail.com>
2023-09-05 16:29:01 +0700

    Creating a write JSON helper method for handling sending a lot of JSON response in the future

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -14,10 +13,8 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"version":     version,
 	}
 
-	// Pass the map to the json.Marshal() function. This returns a []byte slice
-	// containing the encoded JSON. If there was an error, we log it and send the client
-	// a generic error message.
-	js, err := json.Marshal(data)
+	// writeJSON Creating a writeJSON helper method with helpers.go
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(
@@ -25,18 +22,6 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 			"the server encountered a problem and could not process your request",
 			http.StatusInternalServerError,
 		)
-		return
 
 	}
-
-	// Append a newline to the JSON. This is just a small nicety to make it easier to
-	// view in terminal applications.
-	js = append(js, '\n')
-
-	// At this point we know that encoding the data worked without any problems, so we
-	// can safely set any necessary HTTP headers for a successful response.
-	w.Header().Set("Content-Type", "application/json")
-
-	// Use w.Write() to send the []byte slice containing the JSON as the response body.
-	w.Write(js)
 }
