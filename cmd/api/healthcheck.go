092fef3f91b45bae6219ca2403404ba003e9e66e
Ramdoni007 <ramd42156@gmail.com>
2023-09-06 08:48:23 +0700

    add Enveloping response for JSON Structure style guide Structure response JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,16 +5,19 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	// Update Code Sending JSON response to EndPoint healthcheck
-	// Create a map which holds the information that we want to send in the response.
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	// Declare an envelope map containing the data for the response. Notice that the way
+	// we've constructed this means the environment and version data will now be nested
+	// under a system_info key in the JSON response.
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
 	// writeJSON Creating a writeJSON helper method with helpers.go
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(
