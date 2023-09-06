090cb08ea33a1673fead82e6f9f109dc8c8e46df
Ramdoni007 <ramd42156@gmail.com>
2023-09-06 11:36:58 +0700

    Add readJSON method  to handle Error from client Decode error

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -13,13 +12,16 @@ import (
 // return a plain-text placeholder response.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Tittle  string   `json:"tittle"`
+		Title   string   `json:"title"`
 		Year    int32    `json:"year"`
 		Runtime int32    `json:"runtime"`
 		Genres  []string `json:"genres"`
 	}
 
-	err := json.NewDecoder(r.Body).Decode(&input)
+	// Use the new readJSON() helper to decode the request body into the input struct.
+	// If this returns an error we send the client the error message along with a 400
+	// Bad Request status code, just like before.
+	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 		return
