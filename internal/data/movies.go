e6af3f8f88f040fbbbc3a5305cf227513f88ffd8
Ramdoni007 <ramd42156@gmail.com>
2023-09-07 11:30:17 +0700

    Making Making validation rules reusable by creating a Validate function to perform validation

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,13 +1,43 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"github.com/Ramdoni007/21Cinema/internal/validator"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"`
-	Tittle    string    `json:"tittle"`
+	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
 	Runtime   Runtime   `json:"runtime"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+
+// Passing Validation check to func ValidateMovie
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	// Use the Check() method to execute our validation checks. This will add the
+	// provided key and error message to the errors map if the check does not evaluate
+	// to true. For example, in the first line here we "check that the title is not
+	// equal to the empty string". In the second, we "check that the length of the title
+	// is less than or equal to 500 bytes" and so on.
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greather then 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contains at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contains more than 5 genres")
+
+	// Note that we're using the Unique helper in the line below to check that all
+	// values in the input.Genres slice are unique.
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contains duplicate values")
 }
