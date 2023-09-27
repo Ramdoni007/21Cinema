014592cadd83e98f8d6d42fc0732120aee45353a
Ramdoni007 <ramd42156@gmail.com>
2023-09-27 11:53:37 +0700

    Add Functionalitty Gracefull shutdown in myApplication

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +16,7 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 			"version":     version,
 		},
 	}
+	time.Sleep(4 * time.Second)
 
 	// writeJSON Creating a writeJSON helper method with helpers.go
 	err := app.writeJSON(w, http.StatusOK, env, nil)
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"time"
 
@@ -118,30 +115,11 @@ func main() {
 		logger: logger,
 		models: data.NewModel(db),
 	}
-
-	//// Declare  HTTP server with some sensible timeout settings, which listens on the
-	//// port provided in the config struct and uses the server we created above as the
-	//// handler. use the http router instance returned by app.router() as the server handler
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		ErrorLog:     log.New(logger, "", 0),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+	err = app.server()
+	if err != nil {
+		logger.PrintFatal(err, nil)
 	}
 
-	// Again, we use the PrintInfo() method to write a "starting server" message at the
-	// INFO level. But this time we pass a map containing additional properties (the
-	// operating environment and server address) as the final parameter.
-	logger.PrintInfo("Server successfully Start", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-
-	err = srv.ListenAndServe()
-	// Use the PrintFatal() method to log the error and exit.
-	logger.PrintFatal(err, nil)
 }
 
 // The openDB() function returns a sql.DB connection pool.
