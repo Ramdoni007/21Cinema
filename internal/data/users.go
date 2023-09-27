84b9565b84a168d4000e649ed1cfcf26046ff8f8
Ramdoni007 <ramd42156@gmail.com>
2023-09-27 15:18:03 +0700

    Add Migrations database to update version 4 amd add table users and users.go file

---
+++ internal/data/users.go
@@ -0,0 +1,20 @@
+package data
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"version"`
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
